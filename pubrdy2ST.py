ed546e4274ff9c051a5c3def60a17fdd58bf177a
aforres <aforres@users.noreply.github.com>
2024-11-01 18:52:25 +1100

    Update pubrdy2ST.py

--- pubrdy2ST.py
+++ pubrdy2ST.py
@@ -37,3 +37,41 @@ for x in range(len(l)):
         #o.close()
             
             
+
+# ────────────────────── Traceback (most recent call last) ───────────────────────
+
+#   /home/adminuser/venv/lib/python3.12/site-packages/streamlit/runtime/scriptru  
+
+#   nner/exec_code.py:88 in exec_func_with_error_handling                         
+
+                                                                                
+
+#   /home/adminuser/venv/lib/python3.12/site-packages/streamlit/runtime/scriptru  
+
+#   nner/script_runner.py:579 in code_to_exec                                     
+
+                                                                                
+
+#   /mount/src/newsmedia/pubrdy2ST.py:16 in <module>                              
+
+                                                                                
+
+#     13 │   │   finish = x                                                       
+
+#     14 │   │                                                                    
+
+#     15 │   │   story = l[begin+3:finish-1]                                      
+
+#   ❱ 16 │   │   story = story.decode('utf-8').encode('ascii', 'replace')         
+
+#     17 │   │                                                                    
+
+#     18 │   │   #o = open('C:\\tempstore0\\temp' + str(x) +'.txt','w')           
+
+#     19 │   │   for y in range(len(story)):                                      
+
+# ────────────────────────────────────────────────────────────────────────────────
+
+# AttributeError: 'list' object has no attribute 'decode'
+
+# 2024-11-01 07:51:19.198 503 
